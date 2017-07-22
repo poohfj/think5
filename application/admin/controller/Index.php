8971ef7e6b97484363e85f164a96886cd7f9c328
poohfj <5459685@qq.com>
2017-07-22 08:34:59 +0800

    1

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pooh
+ * Date: 2017/6/12
+ * Time: 20:43
+ */
+namespace app\admin\controller;
+class Index{
+    public function index(){
+        return "this is admin Index index";
+    }
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pooh
+ * Date: 2017/6/12
+ * Time: 20:39
+ */
+namespace app\index\controller;
+
+class Index
+{
+
+    public function index(){
+        print_r('this is index Index index');
+    }
+}
